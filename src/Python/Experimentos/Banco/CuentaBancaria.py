3a508c7670fbc3e2c16ef6d0731a1dc21cae1d03
Luis000007 <luisemmanuelguzman20@gmail.com>
2024-11-22 02:16:37 -0600

    Banco

--- src/Python/Experimentos/Banco/CuentaBancaria.py
+++ src/Python/Experimentos/Banco/CuentaBancaria.py
@@ -1,31 +1,28 @@
 import datetime
-
 class CuentaBancaria:
-    def __init__ (self, numero_tarjeta, pin, fecha_vencimiento, saldo):
+    def _init_(self, numero_tarjeta, pin, fecha_vencimiento, saldo_inicial):
         self.numero_tarjeta = numero_tarjeta
         self.pin = pin
         self.fecha_vencimiento = fecha_vencimiento
-        self.saldo = saldo
+        self.saldo = saldo_inicial
 
     def es_tarjeta_valida(self):
-        return self.fecha_vencimiento >= 2024
+        return self.fecha_vencimiento >= 2024 #datetime.date.today().year
 
     def consultar_saldo(self, pin_ingresado):
         if pin_ingresado == self.pin and self.es_tarjeta_valida():
             return self.saldo
-
+        else:
+            print("Algo esta mal :(")
+            return 0.0
 
 def main():
-        #creacion de objetos
-
-    cuenta1 = CuentaBancaria(numero_tarjeta = "123456789", pin = 1234, fecha_vencimiento = 2030, saldo = 1000)
-
-    if cuenta1.es_tarjeta_valida():
-        print(cuenta1.saldo)
-        print(cuenta1.fecha_vencimiento)
-        print(cuenta1.numero_tarjeta)
-        print(cuenta1.pin)
-    else:
-        print("NO")
-
+    #creación de objetos
+    tarjeta1 = CuentaBancaria(numero_tarjeta=12345678910, pin=1234, fecha_vencimiento=2025, saldo_inicial=1000.00)
+    # if  tarjeta1.es_tarjeta_valida():
+    # print("Tarjeta valida :)")
+    #else:
+    #  print("Tarjeta invalida :(")
+    saldo = tarjeta1.consultar_saldo(1234)
+    print(saldo)
 main()
