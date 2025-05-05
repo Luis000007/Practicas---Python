cc460f6bdb7066414c6389201f0e02e4a884d844
Luis000007 <luisemmanuelguzman20@gmail.com>
2025-05-05 00:02:20 -0600

    Bucles - for

---
+++ src/Python/Introduccion/Bucles/For/Hello.py
@@ -0,0 +1,20 @@
+#Definir el número de iteraciones
+for i in range(10):
+    #Declaración que se repite
+    print("Hello")
+
+
+print()
+print("-------------------------------------")
+print()
+
+    ## range() genera una serie de números enteros. La variable i se utiliza para iterar sobre los números.
+## Puedes reemplazar i con cualquier otra variable de tu elección.
+
+for i in range(10):
+    print(i)
+
+print()
+
+for dr in range(3):
+    print(dr)
