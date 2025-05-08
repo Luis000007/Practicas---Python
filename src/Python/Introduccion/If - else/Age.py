0cc4c19c65f8758ffdedd50f36791451c0be630d
Luis000007 <luisemmanuelguzman20@gmail.com>
2025-05-08 00:49:36 -0600

    if - else

---
+++ src/Python/Introduccion/If - else/Age.py
@@ -0,0 +1,21 @@
+age = 22
+
+if age >= 18:
+    print("Regular price")
+
+else:
+    print("Discount")
+
+print()
+print("Proceed to payment")
+
+
+print()
+print()
+
+age = 32
+is_student = True
+if age < 18 or is_student:
+    print("Discount")
+else:
+    print("Regular price")
