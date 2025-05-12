4f7474ad014cdbee288d80db37aaa83f809be2a4
Luis000007 <luisemmanuelguzman20@gmail.com>
2025-05-11 23:47:37 -0600

    Indexion

--- src/Python/Introduccion/Listas/shopping cart.py
+++ src/Python/Introduccion/Listas/shopping cart.py
@@ -6,3 +6,32 @@ shopping_cart = [
 ]
 
 print(shopping_cart)
+
+print()
+print()
+
+#    indexar!
+
+
+last_calls = ["Mom", "Dave", "Lawyer"]
+print(last_calls[0])
+print(last_calls[1])
+
+print()
+print()
+
+"""
+Las listas son mutables. Esto significa que puedes cambiar sus valores incluso después de haber sido creadas.
+"""
+
+nums = [8, 6, 19]
+nums[0] = 1
+nums[1] = 2
+nums[2] = 3
+print(nums)
+
+print()
+print()
+
+words = ["rise", "sun", "glasses"]
+print(words[1] + words[0])
