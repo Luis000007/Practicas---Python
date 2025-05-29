a039a60fc8bc0146cee3a6ba96a54bfe8002615a
Luis000007 <luisemmanuelguzman20@gmail.com>
2025-05-28 21:50:06 -0600

    Funciones

---
+++ src/Python/Introduccion/Funciones/Funciones.py
@@ -0,0 +1,18 @@
+"""
+Las funciones son bloques de código reutilizables para tareas específicas.
+Ayudan a mantener tu código lo más corto y fácil de trabajar posible, evitando que repitas código.
+"""
+
+"""
+Los argumentos van dentro de paréntesis () después del nombre de la función.
+"""
+# Ejemplo:
+
+print("LEEEEEEEGEEEEEENDAAARYYYYYYYYYYYY!!!!!")
+# Funcion: Print
+# Argumento: LEEEEEEEGEEEEEENDAAARYYYYYYYYYYYY!!!!!
+
+
+"""
+print(), type() y range() son ejemplos de funciones incorporadas. Python tiene muchas funciones incorporadas que puedes reutilizar en tu código llamándolas.
+"""
